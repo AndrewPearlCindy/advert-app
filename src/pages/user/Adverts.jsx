142bd6f12683a7a8c96f01380de0fc7909508a72
Pearl-Kai <purlkai92@gmail.com>
2025-03-26 12:11:23 +0000

    update

--- src/pages/user/Adverts.jsx
+++ src/pages/user/Adverts.jsx
@@ -18,7 +18,7 @@ const Adverts = () => {
         {
           id: "s1",
           foodname: "",
-          description: "",
+         description: "",
           price: "",
           image: "/api/placeholder/400/320",
           category: "",
@@ -327,12 +327,15 @@ const Adverts = () => {
                 {/* <p className="text-gray-600 mb-4">By {item.vendor}</p> */}
                 <div className="flex items-center justify-between">
                   <div className="flex items-center">
-                    {/* <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
+                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg> */}
+                    </svg>
                     <span className="ml-1 text-gray-700">{item.rating}</span>
                   </div>
-                  <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg text-sm transition duration-300">
+                  <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg text-sm transition duration-300"
+                  onClick={() => navigate("SingleAd")}
+                  >
+                  
                     View More
                   </button>
                 </div>
